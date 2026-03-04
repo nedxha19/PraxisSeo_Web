fceb31403a3a2363db5bb62294ce2fa6f4f3b314
Gani Hasmegaj <ganhas20@htl-shkoder.com>
2026-03-04 12:40:52 +0100

    dhhdf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,18 @@
 <script>
 import background from '$lib/assets/tek.jpg';
 </script>
+
+<svelte:head>
+    <title>Electronic Devices</title>
+    <meta name="description" content="A collection of electronic devices and gadgets." />
+</svelte:head>
+
+
 <header class="h-[500px] bg-center bg-cover flex items-center justify-center"
  style="background-image: url({background})">
 
 <h1 class="text-5xl">Electronic Devices</h1>
+
 
 </header>
 <main>
